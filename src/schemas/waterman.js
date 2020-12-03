66f2ba1d47a93f0076f36c1615d9ff5156466fd2
Lucas E. Vincent <Lucas.Vincent@entalpi.net>
2020-12-03 19:23:55 +0100

    var -> let

--- src/schemas/waterman.js
+++ src/schemas/waterman.js
@@ -1,9 +1,9 @@
 
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+let Schema = mongoose.Schema;
 
-var WatermanSchema = new Schema({
+let WatermanSchema = new Schema({
     firstName       : {type: String, required:true},
     lastName        : {type: String, required:true},
     passwordHash    : {type: String, required:true},
